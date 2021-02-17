0cec9dec4aa807343f59613b2c757f809f8ce70f
Rafael Montero Muñoz <rmontero@net4things.com>
2021-02-17 17:53:33 +0100

    fix aplicationDbcontext

--- BlazorPeliculas/Server/ApplicationDbContext.cs
+++ BlazorPeliculas/Server/ApplicationDbContext.cs
@@ -45,11 +45,11 @@ namespace BlazorPeliculas.Server
             // PM> Add-migration removerpersonas
             // PM> Update-Database
 
-            //var roleAdminId = "89086180-b978-4f90-9dbd-a7040bc93f41";
-            //var roleAdmin = new IdentityRole()         
-            //    { Id = roleAdminId, Name = "admin", NormalizedName = "admin" };
+            var roleAdminId = "89086180-b978-4f90-9dbd-a7040bc93f41";
+            var roleAdmin = new IdentityRole()         
+                { Id = roleAdminId, Name = "admin", NormalizedName = "admin" };
 
-            //modelBuilder.Entity<IdentityRole>().HasData(roleAdmin);
+            modelBuilder.Entity<IdentityRole>().HasData(roleAdmin);
             // PM> Add-Migration RolAdmin
             // PM> Update-Database
 
