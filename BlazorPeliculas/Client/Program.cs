4c1d10558ffae03f19e754ca323bfb18327cad2f
Rafael Montero Muñoz <rmontero@net4things.com>
2021-02-18 23:48:07 +0100

    Internalization

--- BlazorPeliculas/Client/Program.cs
+++ BlazorPeliculas/Client/Program.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace BlazorPeliculas.Client
 {
@@ -26,8 +29,7 @@ namespace BlazorPeliculas.Client
 
             //// Supply HttpClient instances that include access tokens when making requests to the server project
             //builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorAutenticacion2.ServerAPI"));
-
-
+  
             builder.Services.AddHttpClient<HttpClientConToken>(
                cliente => cliente.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
@@ -36,6 +38,26 @@ namespace BlazorPeliculas.Client
                cliente => cliente.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
             ConfigureServices(builder.Services);
+
+            // load language from localstorage
+            builder.Services.AddLocalization();
+
+            var host = builder.Build();
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            var cultura = await js.InvokeAsync<string>("cultura.get");
+
+            if (cultura == null)
+            {
+                var culturaPorDefecto = new CultureInfo("es-ES");
+                CultureInfo.DefaultThreadCurrentCulture = culturaPorDefecto;
+                CultureInfo.DefaultThreadCurrentUICulture = culturaPorDefecto;
+            }
+            else
+            {
+                var culturaUsuario = new CultureInfo(cultura);
+                CultureInfo.DefaultThreadCurrentCulture = culturaUsuario;
+                CultureInfo.DefaultThreadCurrentUICulture = culturaUsuario;
+            }
 
             await builder.Build().RunAsync();
         }
--- BlazorPeliculas/Shared/Entidades/Genero.cs
+++ BlazorPeliculas/Shared/Entidades/Genero.cs
@@ -8,7 +8,10 @@ namespace BlazorPeliculas.Shared.Entidades
     public class Genero
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(
+            ErrorMessageResourceType = typeof(Resources.Resource), 
+            ErrorMessageResourceName = nameof(Resources.Resource.required))]
         public string Nombre { get; set; }
         public List<GeneroPelicula> GeneroPeliculas { get; set; }
     }
